0664379658a60ad4513a9e3408b6ff499ebd819c
John F Young <john@codeandcreative.com>
2022-12-24 21:22:55 -0800

    adds models

---
+++ models/Trip.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Trip extends Model {}
+
+Trip.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    trip_budget: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      default: 0.0,
+    },
+    traveller_amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      default: 1,
+    },
+    traveller_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "traveller",
+        key: "id",
+      },
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "location",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "location",
+  }
+);
+
+module.exports = Trip;
